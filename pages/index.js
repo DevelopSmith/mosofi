0d32a0f127f8f7f208b278fd3cf8dc203167cd9e
Mohamed Hasan <designsuniverse@gmail.com>
2021-02-26 12:16:05 -0800

    Small Update

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,9 @@ import Link from "next/link";
 
 export default function IndexPage() {
   return (
-    <div>
-      <h1 style={{ textAlign: "center" }}>Mo Sofi</h1>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
+    <div style={{ textAlign: "center" }}>
+      <h1>Mo Sofi</h1>
+      <Link href="/about">About</Link>
     </div>
   );
 }
